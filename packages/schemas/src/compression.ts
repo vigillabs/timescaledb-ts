46894157263a0236b355eee4c761a403ceb2bf6d
danstarns <danielstarns@hotmail.com>
2025-01-10 23:28:05 -0800

    dev: comment *

--- packages/schemas/src/compression.ts
+++ packages/schemas/src/compression.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
+// https://docs.timescale.com/api/latest/compression/hypertable_compression_stats/
+
 export const CompressionStatsSchema = z.object({
   total_chunks: z.number(),
   compressed_chunks: z.number(),
+  // ... TODO: add the rest of the fields
 });
 
 export type CompressionStats = z.infer<typeof CompressionStatsSchema>;
